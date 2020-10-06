c38b634c4c3e67118e0d22836f0d055733d192a2
ltt102858 <821449952@qq.com>
2020-10-06 22:41:49 +0800

    full 1.0

---
+++ HelloDjango-blog-tutorial/blogproject/blog/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+import  markdown
+from django.shortcuts import render,get_object_or_404
+def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def detail(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    md=markdown.Markdown(extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
+    post.body = md.convert(post.body)
+    post.toc=md.toc
+    return render(request, 'blog/detail.html', context={'post': post})
+# Create your views here.
