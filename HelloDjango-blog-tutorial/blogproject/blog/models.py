23dc116a45c3192a78a5357afc1da6693ec9038a
ltt102858 <821449952@qq.com>
2020-10-07 12:14:54 +0800

    debug models

--- HelloDjango-blog-tutorial/blogproject/blog/models.py
+++ HelloDjango-blog-tutorial/blogproject/blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import  timezone
 from django.urls import reverse
+import markdown
+from  django.utils.html import strip_tags
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
